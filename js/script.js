7d212b11f2f4ed99b889e1435d30bc1062434f92
Md Aftab <mda552669@gmail.com>
2022-10-14 23:50:39 +0530

    added comment

--- js/script.js
+++ js/script.js
@@ -33,21 +33,26 @@ function renderTodo(todoItem) {
 
   const item = document.querySelector(`[data-key='${todoItem.id}']`);
 
-  // add this if block
+  //if todoItem contains deleted remove it from DOM
   if (todoItem.deleted) {
-    //console.log(todoItem.id);
     item.remove();
     if (todoItems.length === 0) setTodo.innerHTML = "";
-
+    //don't do anything just get out of the main function
     return;
   }
+
+  //Main logic starts
+
+  //create new node
   const node = document.createElement("li");
 
+  //add todo_item class
   node.setAttribute("class", "todo__item");
+  //add data-key very important
   node.setAttribute("data-key", todoItem.id);
-
+  //separate all the words based on space after
   const highlightArr = todoItem.text.split(/(\s+)/);
-
+  //update the text according to the color
   let updatedText = highlightArr.reduce((acc, cur) => {
     if (cur == todoItem.content) {
       acc += `<span style='color:${todoItem.color};font-weight:500'>${todoItem.content}</span>`;
@@ -56,7 +61,7 @@ function renderTodo(todoItem) {
     }
     return acc;
   }, ``);
-
+  //add the content in the node
   node.innerHTML = `
   <section class="card card--shadow">
   <p>${todoItem.content}</p>
@@ -67,13 +72,15 @@ function renderTodo(todoItem) {
 
   </div>
 </section>`;
+
+  //if the item is already present in the DOM replace it else add it
   if (item) {
     setTodo.replaceChild(node, item);
   } else {
     setTodo.append(node);
   }
 }
-
+//delete function
 function deleteTodo(key) {
   const index = todoItems.findIndex((item) => item.id == Number(key));
   const todo = {
@@ -84,17 +91,22 @@ function deleteTodo(key) {
   renderTodo(todo);
 }
 
+//Update function
 function updateTodo(key) {
   const index = todoItems.findIndex((item) => item.id == Number(key));
   const setForm = Object.values(getForm);
+
+  //set the value of todoItem back to form its hardcoded but will do the job
   setForm[0].value = todoItems[index].content;
   setForm[1].value = todoItems[index].color;
   setForm[2].value = todoItems[index].text;
   setForm[3].id = todoItems[index].id;
   setForm[4].id = todoItems[index].id;
+  //make the form visible
   handleCancel();
 }
 
+//add todoItem
 function addTodoItem(content, color, text, id) {
   let todoItem = {
     content,
@@ -102,18 +114,20 @@ function addTodoItem(content, color, text, id) {
     text,
     id,
   };
-  console.log(id);
+  //if id is already present in case of update just provide it previous id
   if (id) {
     todoItem.id = id;
     todoItems = todoItems.filter((item) => item.id != Number(id));
   } else {
     todoItem.id = Date.now();
   }
-
+  //push into the todoItems dataStructure
   todoItems.push(todoItem);
+  //Show Item on the DOM
   renderTodo(todoItem);
 }
 
+//get data from the form and pass to addTodoItem
 let getData = (e) => {
   e.preventDefault();
 
@@ -122,8 +136,7 @@ let getData = (e) => {
       const value = item.value;
 
       if (item.id) {
-
-        const id=item.id
+        const id = item.id;
         item.id = "";
         return id;
       }
@@ -138,7 +151,9 @@ let getData = (e) => {
   handleCancel();
 };
 
+//call this event went click on save
 save.addEventListener("click", getData);
+
 //restore data after refresh
 document.addEventListener("DOMContentLoaded", (e) => {
   const res = localStorage.getItem("todoData");
@@ -151,9 +166,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
   }
 });
 
-//get hold of clicked todo
+//get hold of Whole todoItems
 const list = document.querySelector(".todo__items");
-//get the event of delete and update
+//get the event of when click on list
 list.addEventListener("click", (event) => {
   //update todo
   if (event.target.classList.contains("btn-update")) {
@@ -164,7 +179,6 @@ list.addEventListener("click", (event) => {
   //delete todo
   if (event.target.classList.contains("btn-delete")) {
     const itemKey = event.target.id;
-    //console.log(itemKey);
     deleteTodo(itemKey);
   }
 });
